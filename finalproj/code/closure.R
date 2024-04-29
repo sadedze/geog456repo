a5285d57deb9526ee1376cb660e6851d1cd1a219
sadedze <sadedze@gmail.com>
2024-04-29 09:09:41 -0400

    1

--- finalproj/code/closure.R
+++ finalproj/code/closure.R
@@ -1,4 +1,8 @@
 library(jsonlite)
+library(tigris)
+library(sf)
+library(dplyr)
+
 df <- read.csv("C:/Users/Senam Adedze/OneDrive - University of North Carolina at Chapel Hill/SP2024/GEOG456/aggregated_closures_06052023.csv")
 
 ## make text into time
@@ -14,7 +18,24 @@ df2$t <- as.numeric(difftime(df2$st, my_start, units = "days"))
 df2$te <- as.numeric(difftime(df2$fn, my_start, units = "days"))
 
 # Convert data to JSON
-json_data <- toJSON(df2)
+#json_data <- toJSON(df2)
 
 # Write JSON to file with .json extension
-write(json_data, "finalproj/data.json")
+#write(json_data, "finalproj/data.json")
+
+# read in nc county shapefile
+nc_cty <- counties(state = "NC")
+
+# pts to shapefile
+pts_sf <- st_as_sf(df2, coords = c("Longitude", "Latitude"), crs = 4326)
+
+#spatial join
+joined_data <- st_join(nc_cty, pts_sf, join = st_intersects) 
+
+# day formatting
+joined_data$day <- date <- as.POSIXct(joined_data$day, format = "%m/%d/%Y")
+
+clean <- joined_data %>% 
+  group_by(NAME,st) %>%
+  filter(st <= st, fn >= fn) %>%
+  summarise(total_observations = n_distinct(IncidentID))
